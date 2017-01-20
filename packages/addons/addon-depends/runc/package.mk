a1c0ee3504df29a654b609779095a800381e14c7
Lukas Rusak <lorusak@gmail.com>
2017-01-20 14:55:24 -0800

    runc: update to 2f7393a

--- packages/addons/addon-depends/runc/package.mk
+++ packages/addons/addon-depends/runc/package.mk
@@ -17,7 +17,7 @@
 ################################################################################
 
 PKG_NAME="runc"
-PKG_VERSION="02f8fa7"
+PKG_VERSION="2f7393a"
 PKG_REV="1"
 PKG_ARCH="any"
 PKG_LICENSE="APL"
