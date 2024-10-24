b271615fba82f1623a74b5a2c97a67fe7d4864fd
Rudi Heitbaum <rudi@heitbaum.com>
2024-10-24 11:12:03 +0000

    pipewire: update to 1.2.6
    News:
    - https://gitlab.freedesktop.org/pipewire/pipewire/-/blob/master/NEWS

--- packages/audio/pipewire/package.mk
+++ packages/audio/pipewire/package.mk
@@ -2,8 +2,8 @@
 # Copyright (C) 2021-present Team LibreELEC (https://libreelec.tv)
 
 PKG_NAME="pipewire"
-PKG_VERSION="1.2.5"
-PKG_SHA256="4c3209c78ce63af6bd2658e5092c220d844fc2b7a039bbbb950e71890cc8ac78"
+PKG_VERSION="1.2.6"
+PKG_SHA256="8d9b4e95dba33d218c760fecbb71672c86a56917f803e96fe6c3af62fa783a95"
 PKG_LICENSE="LGPL"
 PKG_SITE="https://pipewire.org"
 PKG_URL="https://github.com/PipeWire/pipewire/archive/${PKG_VERSION}.tar.gz"
